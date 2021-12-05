fc037cd45f1e236d49a75a1ca1435c09a3ec9d67
Gabethedino <gamergrw2003@gmail.com>
2021-12-05 13:38:39 -0500

    fixed scraper

--- src/scraper.go
+++ src/scraper.go
@@ -20,7 +20,24 @@ func getData(month_ string, year_ string) {
 	checkError(err)
 
 	// This is where you scrape
-	month.Name = doc.Find("#mc_list_01_4050-list-mc-eebc9a21598585f5d8bea2ab08144d58").Text()
+	//month.Name = doc.Find("ul#list-mc-eebc9a21598585f5d8bea2ab08144d58").Text()
+
+	doc.Find("ul#list-mc-eebc9a21598585f5d8bea2ab08144d58").Each(func(i int, ul *goquery.Selection) {
+		ul.Find("li").Each(func(i int, li *goquery.Selection) {
+			id, ok := li.Attr("id")
+			if ok {
+				println(id)
+			}
+			li.Find("div").Each(func(i int, div *goquery.Selection) {
+				var header = (div.Find("div").Find("h3"))
+				alt, ok := header.Find("img").Attr("alt")
+				if ok {
+					println(alt)
+				}
+				println(header.Contents().Text())
+			})
+		})
+	})
 
 	fmt.Println(month.Name)
 }
