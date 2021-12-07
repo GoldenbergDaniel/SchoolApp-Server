a21432ef447a0d363be15d5b0eec10d82e5f7033
Gabethedino <gamergrw2003@gmail.com>
2021-12-07 08:45:39 -0500

    2nd fixed commit

--- src/scraper.go
+++ src/scraper.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -10,46 +11,49 @@ import (
 const url string = "https://www.cavsconnect.com/calendar/"
 const cid string = "mc-eebc9a21598585f5d8bea2ab08144d58"
 
-func getData(month_ string, year_ string) Month {
+func getData(month_ string, year_ string, month *Month) {
 	res, err := http.Get(url + "?cid=" + cid + "&format=list" + "&month=" + string(month_) + "&yr=" + string(year_))
 	checkError(err)
-
-	var month = new(Month)
 
 	defer res.Body.Close()
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkError(err)
 
-	// This is where you scrape
-	//month.Name = doc.Find("ul#list-mc-eebc9a21598585f5d8bea2ab08144d58").Text()
-
-	doc.Find("ul#list-mc-eebc9a21598585f5d8bea2ab08144d58").Each(func(i int, ul *goquery.Selection) {
+	doc.Find("ul#list-mc-eebc9a21598585f5d8bea2ab08144d58").Each(func(_ int, ul *goquery.Selection) {
 		ul.Find("li").Each(func(i int, li *goquery.Selection) {
+			var day = new(Day)
+			fmt.Println(" ")
 			id, ok := li.Attr("id")
 			if ok {
-				month.Name = id
-				println(id)
+				date := id[len(id)-2:]
+				day.Day, err = strconv.Atoi(date)
+				fmt.Println(date)
 			}
-			li.Find("div").Each(func(i int, div *goquery.Selection) {
-				var event = new(Event)
-				var header = (div.Find("div").Find("h3"))
-				alt, ok := header.Find("img").Attr("alt")
-				if ok {
-					event.Category = alt
-					println(alt)
-				}
-				event.Name = header.Contents().Text()
-				println(header.Contents().Text())
 
-				append(*month.Events, *event)
+			li.Find("div").Each(func(a int, div *goquery.Selection) {
+
+				var event = new(Event)
+
+				var header = div.Find("div").Find("h3")
+				alt, ok := header.Find("img").Attr("alt")
+
+				// fmt.Println(a)
+
+				if header.Contents().Text() != "" && header.Contents().Text() != "Even" && header.Contents().Text() != "Odd" {
+					if ok {
+						event.Category = alt
+						fmt.Println(alt)
+
+						event.Name = header.Contents().Text()
+						fmt.Println(header.Contents().Text())
+					}
+					day.Events = append(day.Events, *event)
+				}
 			})
+			month.Days = append(month.Days, *day)
 		})
 	})
-
-	fmt.Println(month.Name)
-
-	return *month
 }
 
 func checkError(err error) {
